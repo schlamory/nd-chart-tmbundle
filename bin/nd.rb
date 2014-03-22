0634cc13b1a8aa08195c0c804eec7c8da6e0ec54
Amory Schlender <amory.schlender@gmail.com>
2014-03-22 07:01:39 -0700

    Moves shell script into its own file, leaving nd as a simple module definition

--- bin/nd.rb
+++ bin/nd.rb
@@ -1,35 +1,5 @@
-#!/usr/bin/ruby
 $:.unshift "~/Library/Application\ Support/Avian/Bundles/ND\ Chart.tmbundle/bin"
 require 'nd/opt_parser'
 require 'nd/patient'
 require 'nd/visit'
 
-def main
-
-  if ARGV[0] == "new" and ARGV[1] == "patient"
-
-    options = Nd::NewPatientOptParser.parse(ARGV)
-    patient = Nd::Patient.new_with_options options
-    if patient.persisted?
-      message = "Patient directory #{patient.dir_path} already exists! Exiting."
-    else
-      patient.save
-      message = "Created new patient file for #{patient.first_name} #{patient.last_name} at #{patient.dir_path}."
-    end
-
-  elsif ARGV[0] == "new" and ARGV[1] == "visit"
-    options = Nd::NewVisitParser.parse(ARGV)
-
-
-  else
-
-    options = Nd::OptParser.parse(ARGV)
-  end
-
-  puts message
-
-end
-
-if __FILE__ == $0
-  main
-end
---
+++ bin/nd_shell.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/ruby
+
+require "~/Library/Application\ Support/Avian/Bundles/ND\ Chart.tmbundle/bin/nd"
+
+
+def main
+
+  if ARGV[0] == "new" and ARGV[1] == "patient"
+
+    options = Nd::NewPatientOptParser.parse(ARGV)
+    patient = Nd::Patient.new_with_options options
+    if patient.persisted?
+      message = "Patient directory #{patient.dir_path} already exists! Exiting."
+    else
+      patient.save
+      message = "Created new patient file for #{patient.first_name} #{patient.last_name} at #{patient.dir_path}."
+    end
+
+  elsif ARGV[0] == "new" and ARGV[1] == "visit"
+    options = Nd::NewVisitParser.parse(ARGV)
+
+
+  else
+
+    options = Nd::OptParser.parse(ARGV)
+  end
+
+  puts message
+
+end
+
+if __FILE__ == $0
+  main
+end
